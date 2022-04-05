79607f73854041beb631df154091c03d4d47ef39
kausik-l <kausik@email.sc.edu>
2022-04-05 13:52:26 -0400

    updated

--- demoRecipeQA/views.py
+++ demoRecipeQA/views.py
@@ -19,6 +19,7 @@ def external(request):
       image=request.FILES.get('image',False)
       print("image is ",image)
       fs=FileSystemStorage()
+      
       if image!= False :
           filename=fs.save(image.name,image)
           fileurl=fs.open(filename)
@@ -26,22 +27,34 @@ def external(request):
           print("file raw url",filename)
           print("file full url", fileurl)
           print("template url",templateurl)
-          out= subprocess.Popen([sys.executable,'     querysys.py',str(fileurl)],shell=False,stdout=subprocess.PIPE)
+          out= subprocess.Popen([sys.executable,'/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA/querysys.py',str(fileurl)],shell=False,stdout=subprocess.PIPE)
           text = out.communicate()[0].decode('utf-8').split("\r\n")
-          print("text",text)
           a = []
           names = []
           images = []
           for each in text:
-                a.append(each[3:].split("/",1)[0])
+                a.append(each[3:].split(" "))
                 
-          for each in a:
+          for each in a[0]:
                 if each!=['']:
-                  names.append(each[0:2][0])
-                  images.append(each[0:2][1])
+                  a[0].remove(each)
                   
-          return render(request,'home.html',{'mainData':out.stdout,'UploadedImage':templateurl,'mainData':out.stdout,'data':names[0],'images':images[0].replace("/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA/demoRecipeQA",""),'data1':names[1],'images1':images[1].replace("/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA/demoRecipeQA",""),'data2':names[2],'images2':images[2].replace("/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA/demoRecipeQA","")})
-    
+          for each in a[0][1::3]:
+                names.append(each)      
+
+                
+          for each in a[0][0::3]:
+                images.append(each.replace("/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA",""))
+
+          tup = []
+          for (name,image) in zip(names,images):
+                tup.append((name,image))
+          number = len(names)
+                
+          #return render(request, 'home.html',{'mainData':a[0],'recipes':tup}) 
+          return render(request,'home.html',{'UploadedImage':templateurl,'mainData':a,'recipes': tup, 'length':number})
+
+       
       else:
           out= subprocess.Popen([sys.executable,'/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA/querysys.py',inp],stdout=subprocess.PIPE)
           text = out.communicate()[0].decode('utf-8').split("\r\n")
@@ -55,8 +68,6 @@ def external(request):
                 if each == '':
                       a[0].remove(each)
       
-
-                
           for each in a[0][1::3]:
                 images.append(each.replace("/Users/kausiklakkaraju/Documents/researchPhd/projects/work/MMReasoningDemo-main/demo/demoRecipeQA",""))
                 
@@ -64,7 +75,8 @@ def external(request):
                 names.append(each)      
                 
           tup = []
+          number = len(names)
           for (name,image) in zip(names,images):
                 tup.append((name,image))
   
-          return render(request, 'home.html',{'mainData':a[0],'recipes':tup})
+          return render(request, 'home.html',{'text':inp,'mainData':a[0],'recipes':tup, 'length':number})
